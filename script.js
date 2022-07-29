518a2c234227447641fbb8a63f75f0e31351ddfe
hassanbaryal <h.baryal2@gmail.com>
2022-07-29 18:43:25 -0400

    Added function to check whether any players have won the game

--- script.js
+++ script.js
@@ -4,30 +4,48 @@ const controller = (() => {
     let activePlayer = 1;
     //check active player
 
-    const checkActivePlayer = () => {return activePlayer};
+    const getActivePlayer = () => {return activePlayer};
 
     //if player one is active, change to player two, else vice versa
     const changeActivePlayer = () => {
         activePlayer == 1 ? activePlayer = 2 : activePlayer = 1
     };
 
-    //update info board
+    // UPDATE INFO BOARD
 
-    return {checkActivePlayer, changeActivePlayer};
+    // CHECK WIN CONDITION
+
+    //array = [[0,1,2], [3,4,5], [6,7,8],...]
+    // use array.some? use an array method that gets an array item that fits a criteria (i.e. win condition)
+    // filter, findIndex (I think findIndex would be best)
+    const checkWinCondition = (gameCells) => {
+        let winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+        let index = winConditions.findIndex(condition => (gameCells[condition[0]] == gameCells[condition[1]] && gameCells[condition[0]] == gameCells[condition[2]]));
+
+        if (index !== -1) {
+            console.log("Won!! " + winConditions[index]);
+        };
+    };
+
+    return {getActivePlayer, changeActivePlayer, checkWinCondition};
 })();
 
 const gameBoard = (function () {
+     
+    let gameCells = [0,1,2,3,4,5,6,7,8];
+
+    // History array
+    let gameHistory = [];
 
     const board = Array.from(document.querySelectorAll(".game-cell"));
     board.forEach(cell => {
         cell.addEventListener("click", e => {
-            console.log(e.target)
-            placeMarker(e.target, controller.checkActivePlayer());
+            placeMarker(e.target, controller.getActivePlayer());
         });
     });
 
-    //history array
-    let gameHistory = [];
+    
 
     //place marker function
     const placeMarker = (cell, activePlayer) => {
@@ -35,19 +53,22 @@ const gameBoard = (function () {
         if (cell.classList.contains("placed")) {
             //update info board to say u cant do taht, marker is already placed there
         } else {
-
-            if (activePlayer == 1) {
-                //Place X for player one
-                cell.querySelector(".X").classList.toggle("inactive");
-            } else {
-                cell.querySelector(".O").classList.toggle("inactive");
-            };
+            // Place Marker
+            activePlayer == 1 ? cell.querySelector(".X").classList.toggle("inactive") : cell.querySelector(".O").classList.toggle("inactive");
 
             // Disable the cell
             cell.classList.toggle("placed");
 
-            // Change active player after placing marker
+            // Add cell to game history
+            activePlayer == 1 ? gameCells[cell.dataset.cell - 1] = 'X' : gameCells[cell.dataset.cell - 1] = 'O';
+            gameHistory.push(cell.dataset.cell);
+
+            // Change active player
             controller.changeActivePlayer();
+
+            // Check win conditions
+            if (gameHistory.length >= 5) controller.checkWinCondition(gameCell);
+            
         };
         
     };
@@ -56,10 +77,10 @@ const gameBoard = (function () {
     return {placeMarker}
 })();
 
+// CREATE PLAYER TWO WITH MULTIPLE IF ELSE
+// THAT CHECKS STATE OF PLAYER TWO (i.e if tis easy, medium, etc.) AND EXECUTES AI STUFF OR NOTHING IF CUSTOM PLAYER TWO
+// AI will click one of the cells
 const createPlayer = () => {
     let playerCells = []
 
-    const checkWinCondition = () => {
-
-    }
 }
