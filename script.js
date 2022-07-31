9658ac57dd402143017a3f9200d207ae4214ca18
hassanbaryal <h.baryal2@gmail.com>
2022-07-31 19:01:21 -0400

    Added functionality to undo button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// Undo button
+// Restart button
+// Update Info Board, maybe "player X has placed in cell 4", "You can't do that!!", "Player has won!"
+// AI, playerTwo function
+
 
 
 const controller = (() => {
     let activePlayer = 1;
-    //check active player
+
+    const undoBtn = document.querySelector(".undo");
+    undoBtn.addEventListener("click", () => {gameBoard.deleteMarker()});
 
     const getActivePlayer = () => {return activePlayer};
 
@@ -16,18 +23,15 @@ const controller = (() => {
     // UPDATE INFO BOARD
 
     // CHECK WIN CONDITION
-
-    //array = [[0,1,2], [3,4,5], [6,7,8],...]
-    // use array.some? use an array method that gets an array item that fits a criteria (i.e. win condition)
-    // filter, findIndex (I think findIndex would be best)
     const checkWinCondition = (gameCells) => {
         let winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
         let index = winConditions.findIndex(condition => (gameCells[condition[0]] == gameCells[condition[1]] && gameCells[condition[0]] == gameCells[condition[2]]));
 
         if (index !== -1) {
-            gameBoard.toggleGameBoard(0)
-            gameBoard.bounceMarkers(winConditions[index])
+            gameBoard.toggleGameBoard(0);
+            gameBoard.bounceMarkers(winConditions[index]);
+            undoBtn.disabled = true
         };
     };
 
@@ -43,7 +47,7 @@ const gameBoard = (function () {
     // History array
     let gameHistory = [];
 
-    const board = document.querySelectorAll(".game-cell");
+    const board = Array.from(document.querySelectorAll(".game-cell"));
     board.forEach(cell => {
         cell.addEventListener("click", e => {
             if(e.target.nodeName == "DIV") placeMarker(e.target, controller.getActivePlayer());
@@ -80,21 +84,38 @@ const gameBoard = (function () {
 
     //delete marker function
 
+    const deleteMarker = () => {
+        console.log(gameHistory);
+        if (gameHistory.length === 0) {
+            // make info board say you cant detelete something that hasnt happened yet
+            // "You cannot delete the future" or something
+        } else {
+            const targetCell = board.find(cell => cell.dataset.cell == gameHistory[gameHistory.length - 1]);
+            Array.from(targetCell.children).forEach(marker => {
+                if (!marker.classList.contains("inactive")) marker.classList.toggle("inactive");
+            });
+            targetCell.classList.toggle("placed")
+            gameHistory.pop();
+            controller.changeActivePlayer();
+            // UPDATE INFO BOARD TO SAY UNDOED CELL
+        };
+    };
+
     // Bounce markers
     const bounceMarkers = (markers) => {
-        let targets = []
+        let targets = [];
 
         // Toggle 'bounce' class to make winning markers bounce
         markers.forEach(marker => {
-            let target = Array.from(board[marker].children)
-            target = target.filter(xo => !xo.classList.contains("inactive"))
-            target[0].classList.toggle("bounce")
-            targets.push(target[0])
-        })
+            let target = Array.from(board[marker].children);
+            target = target.filter(xo => !xo.classList.contains("inactive"));
+            target[0].classList.toggle("bounce");
+            targets.push(target[0]);
+        });
 
         // Remove 'bounce' class from winning markers
-        setTimeout(() => {targets.forEach(target => {target.classList.toggle("bounce")})}, 500)
-    }
+        setTimeout(() => {targets.forEach(target => {target.classList.toggle("bounce")})}, 500);
+    };
 
     const toggleGameBoard = (state) => {
         if (state == 0) {
@@ -109,7 +130,7 @@ const gameBoard = (function () {
         gameState = state;
     };
     
-    return {placeMarker, bounceMarkers, toggleGameBoard}
+    return {placeMarker, deleteMarker, bounceMarkers, toggleGameBoard}
 })();
 
 // CREATE PLAYER TWO WITH MULTIPLE IF ELSE
